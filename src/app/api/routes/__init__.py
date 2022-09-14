75e7aa3c17a75c8ccf9b959c4acefd3241e2dd89
Jorge Luis <jorgeluis.jimenez@xmartlabs.com>
2022-09-14 12:28:03 -0300

    added create user feature

--- src/app/api/routes/__init__.py
+++ src/app/api/routes/__init__.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from app.api.routes.ping import router as ping_router
+from app.api.routes.users import router as users_router
 
 
 router = APIRouter()
 
 
 router.include_router(ping_router)
+router.include_router(users_router)
---
+++ src/app/db/repositories/users.py
@@ -0,0 +1,35 @@
+from typing import List
+
+from app.core.models import UUIDModel
+from app.db.repositories.base import BaseRepository
+from app.user.crud import UserCreate
+from app.user.models import UserBase, UserRead, UserInDB
+
+GET_USERS_QUERY = """
+    SELECT uuid, nickname, role, created_at, updated_at
+    FROM users;
+"""
+
+CREATE_USER_QUERY = """
+    INSERT INTO users (nickname, role)
+    VALUES (:nickname, :role)
+    RETURNING uuid, nickname, role;
+"""
+
+
+class UsersRepository(BaseRepository):
+    """"
+    All database actions associated with the User resource
+    """
+
+    async def create_user(self, *, new_user: UserCreate) -> UserInDB:
+        query_values = new_user.dict()
+        user = await self.db.fetch_one(query=CREATE_USER_QUERY, values=query_values)
+
+        return UserInDB(**user)
+
+    async def get_users(self) -> List[UserRead]:
+        user_records = await self.db.fetch_all(query=GET_USERS_QUERY)
+
+        return [UserRead(**u) for u in user_records]
+
