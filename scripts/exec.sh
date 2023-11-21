1761a0077d08b991a86b29e202c26f64c8631b85
Jorge Artave <jorge@xmartlabs.com>
2023-11-21 12:50:29 -0300

    Allow exec script to be used outside script folder and check docker compose

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -1,29 +1,50 @@
 #!/bin/bash
 
-DOCKER_COMPOSE_BASE_FILE_PATH="${PWD%/*}/docker-compose.yaml"
-TEST_DOCKER_COMPOSE_FILE_PATH="${PWD%/*}/docker-compose.test.yaml"
+if [ -e "${PWD%/*}/docker-compose.yaml" ]; then
+    DOCKER_FILES_PATHS="${PWD%/*}"
+    SCRIPTS_PATH="."
+elif [ -e "${PWD%}/docker-compose.yaml" ]; then
+    DOCKER_FILES_PATHS="${PWD%}"
+    SCRIPTS_PATH="./scripts"
+else
+    echo "docker-compose related files not found"
+    exit 1
+fi
+
+if command -v docker-compose &> /dev/null; then
+    DOCKER_COMMAND="docker-compose"
+elif command -v docker compose  &> /dev/null; then
+    DOCKER_COMMAND="docker compose"
+else
+    echo "Neither docker-compose nor docker compose are available."
+    exit 1
+fi
+
+
+DOCKER_COMPOSE_BASE_FILE_PATH="${DOCKER_FILES_PATHS}/docker-compose.yaml"
+TEST_DOCKER_COMPOSE_FILE_PATH="${DOCKER_FILES_PATHS}/docker-compose.test.yaml"
 DOCKER_COMPOSE_TEST_FILES="-f $DOCKER_COMPOSE_BASE_FILE_PATH -f $TEST_DOCKER_COMPOSE_FILE_PATH"
 
 
 case "$1" in
     format)
-        docker-compose run -T backend bash < format.sh
+        $DOCKER_COMMAND run -T backend bash < "${SCRIPTS_PATH}/format.sh"
         ;;
     makemigrations)
-        docker-compose run -T backend bash < makemigrations.sh
+        $DOCKER_COMMAND run -T backend bash < "${SCRIPTS_PATH}/makemigrations.sh"
         ;;
     migrate)
-        docker-compose run -T backend bash < migrate.sh
+        $DOCKER_COMMAND run -T backend bash < "${SCRIPTS_PATH}/migrate.sh"
         ;;
     bash)
-        docker-compose run backend bash
+        $DOCKER_COMMAND run backend bash
         ;;
     shell)
-        docker-compose run --entrypoint python backend src/helpers/shell.py
+        $DOCKER_COMMAND run --entrypoint python backend src/helpers/shell.py
         ;;
     test)
-        docker-compose $DOCKER_COMPOSE_TEST_FILES run --rm -T backend bash < test.sh
-        docker-compose $DOCKER_COMPOSE_TEST_FILES down
+        $DOCKER_COMMAND $DOCKER_COMPOSE_TEST_FILES run --rm -T backend bash < "${SCRIPTS_PATH}/test.sh"
+        $DOCKER_COMMAND $DOCKER_COMPOSE_TEST_FILES down
         ;;
     *)
         echo "Usage: $0 {format|makemigrations|migrate|bash|shell|test}"
