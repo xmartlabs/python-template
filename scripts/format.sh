e473372b329db1585203d92ff55526bb5c05176c
Miguel Revetria <miguel@xmartlabs.com>
2025-02-03 10:01:12 -0300

    chore: update dependencies

--- scripts/format.sh
+++ scripts/format.sh
@@ -4,7 +4,7 @@ printf "Runing pycln...\n"
 poetry run python -m pycln src --exclude __init__.py --all
 
 printf "\nRunning isort...\n"
-poetry run python -m run isort src
+poetry run python -m isort src
 
 printf "\nRunning flake8...\n"
 poetry run python -m flake8 src
