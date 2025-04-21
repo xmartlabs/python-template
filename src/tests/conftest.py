14ac4e9422abfb15a632a6fb8725e24ae34af2c7
Gaston Valvassori <gaston.valvassori@xmartlabs.com>
2025-04-21 09:07:59 -0300

    docs: translate to english

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -30,7 +30,7 @@ async def async_client() -> AsyncGenerator:
 
 @pytest.fixture(scope="session")
 def event_loop() -> Generator:
-    """Forzar pytest-asyncio a usar un solo loop por sesión"""
+    """Force pytest to use a new event loop for each test."""
     loop = asyncio.get_event_loop_policy().new_event_loop()
     yield loop
     loop.close()
