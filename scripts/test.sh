50077063d2c85a062f8999c198e91d35522bf9bb
Miguel Revetria <miguel@xmartlabs.com>
2025-05-26 12:34:52 -0300

    chore: using dedicated db for testing (#49)

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-poetry run pytest src
+set -e
+pwd
+# Ensure test database schema is up-to-date
+
+TEST_DB_NAME="test"
+TEST_DB_CONN_STRING="dev:dev@postgres:5432"
+
+setup_test_database()
+{
+    echo "Setting up test database"
+    DATABASE_URL="postgresql://$TEST_DB_CONN_STRING"
+    psql $DATABASE_URL -c "DROP DATABASE IF EXISTS $TEST_DB_NAME"
+    psql $DATABASE_URL -c "CREATE DATABASE $TEST_DB_NAME"
+    psql $DATABASE_URL -c "GRANT ALL PRIVILEGES ON DATABASE $TEST_DB_NAME TO dev"
+    DATABASE_URL="$DATABASE_URL/$TEST_DB_NAME" ./scripts/migrate.sh
+}
+
+setup_test_database
+ASYNC_DATABASE_URL=postgresql+asyncpg://$TEST_DB_CONN_STRING/$TEST_DB_NAME poetry run pytest src
