4be183fabc957a17cee61594bc1b45f355c47f5c
Nicolas Fripp <nicolasfripp@Nicolass-MacBook-Pro.local>
2025-08-07 11:30:53 -0300

    script added

---
+++ scripts/rename-template.py
@@ -0,0 +1,39 @@
+# in all files under src, replace the string "mi-proyecto" with the name of the project, coming from params.
+
+import argparse
+import glob
+import os
+
+paths_to_replace = [
+    "src/**/*.py",
+    "tests/**/*.py",
+    "Dockerfile",
+    ".env.example",
+    ".devcontainer/**/*",
+    "uv.lock",
+    "pyproject.toml",
+    ".github/workflows/python-app.yml",
+    "scripts/**/*",
+    "scripts/**/*",
+
+]
+
+def rename_template(new_project_name: str):
+    # get all files, ecl
+    for path in paths_to_replace:
+        files = glob.glob(path, recursive=True)
+        for file in files:
+            if not os.path.isfile(file) or file.__contains__("rename-template.py"):
+                continue
+            with open(file, "r") as f:
+                content = f.read()
+            content = content.replace("python-template", new_project_name)
+            content = content.replace("python_template", new_project_name.replace("-", "_"))
+            with open(file, "w") as f:
+                f.write(content)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("new_project_name", type=str)
+    args = parser.parse_args()
+    rename_template(args.new_project_name)
